91e31c94d83a3869116999d81831ea593efb391e
nisheed.jagadish <nisheed.jagadish@loblaw.ca>
2021-03-29 06:53:18 -0400

    Added comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
+// The tailwind CSS config: https://tailwindcss.com/docs/configuration
+// Update the config and extend it with custom value and variables
 module.exports = {
 	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {
+      // Add wayfair colors
       colors: {
         'wf-gray': '#BBBBBF',
         'wf-yellow': '#F8CF1C',
         'wf-purple': '#7F187F',
       },
       fontSize: {
+        // Add SVG icon sizing
         'wf-icon': '18px',
       },
       spacing: {
+        // Common widths which are not available in tailwind CSS
         'wf-icon': '18px',
         'wf-menu': '200px',
       }
